ad8db57d219839a7bfe125c56c42409c81d22288
MikhaelaRoy <149487064+MikhaelaRoy@users.noreply.github.com>
2023-11-06 14:25:09 +0000

    Update milestone_2.py
    Adding comments

--- milestone_2.py
+++ milestone_2.py
@@ -1,13 +1,18 @@
+#Using library to randomise the word choice in the list. 
 import random 
 
+#Creatign a list of words
 word_list = ['Apple','Banana','Guava','Mango','Orange']
 print(word_list)
 
-guess = input('Please enter a singular letter: ')
-
+#Choosing a random word from the list
 word = random.choice(word_list)
 print(word)
 
+#User engagement to play the game
+guess = input('Please enter a singular letter: ')
+
+#Checking if the word meets the condition that the user has input. 
 if len(guess) == 1 and guess.isalpha():
     print('Good Guess')
 else:
